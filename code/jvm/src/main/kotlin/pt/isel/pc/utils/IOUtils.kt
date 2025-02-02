bb35d32e82712ee645a52dc950c4d24ea9293fac
Pedro Felix <pmhsfelix@gmail.com>
2025-02-02 16:33:46 +0000

    adds code/jvm

---
+++ code/jvm/src/main/kotlin/pt/isel/pc/utils/IOUtils.kt
@@ -0,0 +1,12 @@
+package pt.isel.pc.utils
+
+import java.io.BufferedWriter
+
+fun BufferedWriter.writeLine(
+    format: String,
+    vararg values: Any?,
+) {
+    write(String.format(format, *values))
+    newLine()
+    flush()
+}
